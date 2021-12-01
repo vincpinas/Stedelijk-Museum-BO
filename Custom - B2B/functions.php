2c4ab6ed62a44a8c5ca7f3d7c68c38fba0bca70a
vincent <ikbengerrit92@gmail.com>
2021-12-01 10:09:00 +0100

    :bug: small syntax fix

--- Custom - B2B/functions.php
+++ Custom - B2B/functions.php
@@ -1,9 +1,9 @@
 <?php 
 
 function art_load_style_scripts(){
-    wp_enqueue_style('main.css', get_template_directory_url() . '/css/thema.css');
+    wp_enqueue_style('main.css', get_template_directory_uri() . '/css/thema.css');
 }
 
-add_action('wp_enqueue_styles', 'art_load_style_scripts');
+add_action('wp_enqueue_scripts', 'art_load_style_scripts');
 
 ?>
