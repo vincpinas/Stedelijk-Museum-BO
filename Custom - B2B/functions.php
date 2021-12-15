b3a52042bbeb254caebca6cf067167f90c2d9792
svmcd <spolat0750@gmail.com>
2021-12-15 14:31:03 +0100

    :blue_heart: added font & converted css to scss

--- Custom - B2B/functions.php
+++ Custom - B2B/functions.php
@@ -1,7 +1,8 @@
 <?php 
 
 function art_load_style_scripts(){
-    wp_enqueue_style('main.css', get_template_directory_uri() . '/css/thema.css');
+    wp_enqueue_style('main.css', get_template_directory_uri() . '/css/global.css');
+    wp_enqueue_style('main.css', get_template_directory_uri() . '/css/style.css');
 }
 
 add_action('wp_enqueue_scripts', 'art_load_style_scripts');
--- Custom - B2B/header.php
+++ Custom - B2B/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 
-<head>
+<head class="navbar" >
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <title><?php bloginfo( 'name' ); ?></title>
     <?php wp_head(); ?>
